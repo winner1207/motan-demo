632f6c25f9ca38cc81c5c5a2b3558d86f5875581
wangyitao <182443045@qq.com>
2018-11-28 14:49:58 +0800

    spring boot基础代码

---
+++ chatbusiness/src/main/java/com/wyt/motan/chatbusiness/ChatbusinessApplication.java
@@ -0,0 +1,12 @@
+package com.wyt.motan.chatbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbusinessApplication.class, args);
+    }
+}
