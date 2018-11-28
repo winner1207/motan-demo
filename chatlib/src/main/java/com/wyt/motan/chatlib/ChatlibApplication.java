c9f4867181a9cecf84ff4a47c00a1af34d379217
wangyitao <182443045@qq.com>
2018-11-28 15:04:56 +0800

    spring boot基础代码-2

---
+++ chatlib/src/main/java/com/wyt/motan/chatlib/ChatlibApplication.java
@@ -0,0 +1,12 @@
+package com.wyt.motan.chatlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatlibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatlibApplication.class, args);
+    }
+}
